c838c0b935c1cccca8ed9be240178fd72375ae49
Santiago Herrera Muñoz <santiago.herrera2@utp.edu.co>
2025-09-25 17:49:20 -0500

    explorando vue3

--- hola-mundo-pa-yisus/src/components/HelloWorld.vue
+++ hola-mundo-pa-yisus/src/components/HelloWorld.vue
@@ -10,19 +10,14 @@ defineProps({
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
   </div>
 </template>
 
 <style scoped>
 h1 {
   font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
+  font-size: 4rem;
+  position:  relative;
   top: -10px;
 }
 
